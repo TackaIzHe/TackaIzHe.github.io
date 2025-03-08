964a7591474a5f09d8f8b50c749b625e066fc809
TackaIzHe <144840948+TackaIzHe@users.noreply.github.com>
2025-03-08 16:16:09 +0000

    add

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", (context) => {
+app.MapGet("/52", (context) => {
     context.Response.Cookies.Append("Hello","52");
     return Task.CompletedTask;
 });
